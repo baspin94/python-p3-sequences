68085fb59a7a879b9dcaf9e77f736c87472d8881
Bianca Aspin <bianca@mj94.net>
2023-02-21 09:13:30 -0800

    Complete lab.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 
+            89,144,233,377,610,987, 1597, 2584, 4181, 6765, 
+            10946, 17711, 28657, 46368, 75025, 121393, 196418, 
+            317811, 514229]
+
 def print_fibonacci(length):
-    pass
+    print(fibonacci[:length])
